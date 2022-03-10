e22b7276ab65efe1591cef9f8020bde69a85961b
Jackson Dolman <mail@dolmios.com>
2022-03-10 18:40:04 +1100

    fix: resolve checkbox tooltip triggering state change

--- src/components/Input/Input.Checkbox.tsx
+++ src/components/Input/Input.Checkbox.tsx
@@ -15,11 +15,12 @@ export interface Props {
   id?: string;
   size?: 1 | 2;
   tooltip?: string;
+  type?: 'hover' | 'click';
 }
 
 const { CheckboxWrapper, CheckboxLabelWrapper, CheckboxTooltipWrapper } = InputStyles();
 
-export default function Checkbox({ checked, children, css, disabled, id, size = 1, tooltip }: Props): JSX.Element {
+export default function Checkbox({ checked, children, css, disabled, id, size = 1, tooltip, type = 'hover' }: Props): JSX.Element {
   const [isChecked, setIsChecked] = useState(checked as boolean);
 
   const handleChange = (): void => {
@@ -30,7 +31,7 @@ export default function Checkbox({ checked, children, css, disabled, id, size = 
   };
 
   return (
-    <CheckboxWrapper css={css} disabled={disabled} id={id} onClick={handleChange}>
+    <CheckboxWrapper css={css} disabled={disabled} id={id}>
       <Button
         css={{
           display: 'flex',
@@ -39,13 +40,16 @@ export default function Checkbox({ checked, children, css, disabled, id, size = 
           borderRadius: '$3',
           aspectRatio: 1,
           padding: '$2',
-        }}>
+        }}
+        onClick={handleChange}>
         {isChecked ? <Check /> : <Circle opacity={0.1} />}
       </Button>
-      <CheckboxLabelWrapper size={size}>{children}</CheckboxLabelWrapper>
+      <CheckboxLabelWrapper onClick={handleChange} size={size}>
+        {children}
+      </CheckboxLabelWrapper>
       {tooltip && (
         <CheckboxTooltipWrapper>
-          <Tooltip passKey={`${children}`} trigger={<Question size={18} />} type='click'>
+          <Tooltip passKey={`${children}`} trigger={<Question size={18} />} type={type}>
             {tooltip}
           </Tooltip>
         </CheckboxTooltipWrapper>
