a5a0f2386b57f772f49c798c05a32dc082181880
Jackson Dolman <mail@dolmios.com>
2023-01-12 15:55:02 -0500

    refactor: change build directories

--- build/components/Image/Image.styles.js
+++
@@ -1,8 +0,0 @@
-import { styled } from "../../stitches.config";
-export var ImageStyled = styled("div", {
-    height: "100%",
-    position: "relative",
-    transition: "$default",
-    width: "100%"
-});
-export default ImageStyled;
