db2e2b67f9f685983d83755a11e6ef9cfbb4b2da
Jackson Dolman <mail@dolmios.com>
2023-08-31 22:14:00 -0400

    fix: local storage pre-render error

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,6 +1,10 @@
 import { useState, useEffect } from "react";
 
 function getStorageValue<T>(key: string, defaultValue: T): T {
+  if (typeof window === 'undefined') {
+    return defaultValue;
+  }
+
   const saved = localStorage.getItem(key);
 
   if (saved === null) {
@@ -11,11 +15,14 @@ function getStorageValue<T>(key: string, defaultValue: T): T {
 }
 
 export default function useLocalStorage<T>(key: string, defaultValue: T): [T, (value: T) => void] {
-  const [value, setValue] = useState(() => {
+  const [value, setValue] = useState<T>(() => {
     return getStorageValue(key, defaultValue);
   });
 
   useEffect(() => {
+    if (typeof window === 'undefined') {
+      return;
+    }
     localStorage.setItem(key, JSON.stringify(value));
   }, [key, value]);
 
--- src/hooks/useOutsideClick.ts
+++ src/hooks/useOutsideClick.ts
@@ -1,17 +1,41 @@
-import { RefObject } from "react";
+/* eslint-disable no-console */
+import { useState, useEffect } from "react";
 
-import { useEventListener } from "../index";
+function getStorageValue<T>(key: string, defaultValue: T): T {
+  if (typeof window === 'undefined') {
+    return defaultValue;
+  }
 
-export default function useOutsideClick<T extends HTMLElement = HTMLElement>(
-  ref: RefObject<T>,
-  handler: (event: MouseEvent | TouchEvent) => void,
-  mouseEvent: "mousedown" | "mouseup" = "mousedown",
-): void {
-  useEventListener(mouseEvent, (event: MouseEvent | TouchEvent) => {
-    if (!ref.current || ref.current.contains(event.target as Node)) {
-      return;
+  const saved = localStorage.getItem(key);
+
+  try {
+    if (saved) {
+      return JSON.parse(saved);
     }
 
-    handler(event);
-  });
+    return defaultValue;
+  } catch (error) {
+    console.error(`Error parsing localStorage key “${key}”:`, error);
+
+    return defaultValue;
+  }
+}
+
+export default function useLocalStorage<T>(key: string, defaultValue: T): [T, (value: T) => void] {
+  const [value, setValue] = useState<T>(defaultValue);
+
+  useEffect(() => {
+    const initialValue = getStorageValue(key, defaultValue);
+
+    setValue(initialValue);
+  }, [key, defaultValue]);
+
+  useEffect(() => {
+    if (typeof window === 'undefined') {
+      return;
+    }
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
 }
