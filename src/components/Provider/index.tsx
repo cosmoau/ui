aae56fb0d084d9870d18140288ab51ffb29980f8
Jackson Dolman <mail@dolmios.com>
2024-07-31 14:01:16 -0400

    chore: bump deps

--- src/components/Provider/index.tsx
+++ src/components/Provider/index.tsx
@@ -20,9 +20,7 @@ export default function Provider({ children, css, dark }: IProvider): JSX.Elemen
   }, []);
 
   return (
-    <ProviderStyled
-      className={dark ? darkTheme : ""}
-      css={css}>
+    <ProviderStyled className={dark ? darkTheme : ""} css={css}>
       <IconContext.Provider
         value={{ alignmentBaseline: "middle", height: 20, weight: "duotone", width: 20 }}>
         <ToastController />
